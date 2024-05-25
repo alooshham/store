042c08ba69d8fb5055858ed5cddd4de324fd3213
alooshham <mohammadhameshli87@gmail.com>
2024-05-26 00:53:13 +0300

    upload website files

---
+++ java.js
@@ -0,0 +1,10 @@
+document.querySelector('.heading').style.color = '#ff5777';
+
+
+
+let container = document.querySelector('.container');
+
+
+function colors(color){
+    container.style.background = color; 
+}
